6be300d4766c96119fa8c7beeeb073b129878985
Daniclo <danielcollfdz@gmail.com>
2023-10-19 17:41:16 +0200

    Corregida tarea 1 Unidad 2 Daniel Coll

--- src/app/Ejer8.java
+++ src/app/Ejer8.java
@@ -4,6 +4,7 @@ import model.Login;
 import mysql.LoginHandleDB;
 import util.DatabaseConnection;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -14,6 +15,17 @@ public class Ejer8 {
         Login login = logins.get(0);
         System.out.println(login);
         LoginHandleDB.updateLogin(login);
-        System.out.println(login);
+        System.out.println("\nComprobación:");
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from login where id=1");
+            while (rs.next()){
+                System.out.println(rs.getLong("id") + " " + rs.getString("username") +
+                        " " +rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getSQLState() + " " + e.getMessage());
+        }
     }
 }
